04321b78efc9758afbabecf2a813d4ff651008e3
ramia15 <53275043+ramia15@users.noreply.github.com>
2023-09-02 11:49:21 -0500

    Migrated code in server.js to work with OpenAI API v4

--- dream/server.js
+++ dream/server.js
@@ -1,13 +1,11 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI,
 });
-
-const openai = new OpenAIApi(configuration);
 
 import express from 'express';
 import cors from 'cors';
@@ -20,18 +18,28 @@ app.post('/dream', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openai.images.generate({
       prompt,
       n: 1,
       size: '1024x1024',
     });
+    console.log(aiResponse.data);
+    const image = aiResponse.data[0].url;
+    console.log(image);
 
-    const image = aiResponse.data.data[0].url;
-    res.send({ image });
+    res.send({ image }); // Sends the response to the client
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    if (error instanceof OpenAI.APIError) {
+      console.error(error.status);  // e.g. 401
+      console.error(error.message); // e.g. The authentication token you passed was invalid...
+      console.error(error.code);    // e.g. 'invalid_api_key'
+      console.error(error.type);    // e.g. 'invalid_request_error'
+    } else {
+      console.error(error)
+      res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    }
   }
 });
+
 
 app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
